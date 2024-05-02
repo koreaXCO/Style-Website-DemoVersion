0634512d4ab28506e60ad485fc00bc3148979f3d
배지원 <wldnjs7073@naver.com>
2024-05-03 02:12:24 +0900

    채팅서비스 구현완료(디자인수정필요)

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,10 +157,5 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
-//전송버튼 스크립트
-document.querySelector('#send').addEventListener('click', function(){
-  var template = `<div class="line">
-      <span class="chat-box">${document.querySelector('#input').value }</span>
-  </div>`;
-  document.querySelector('.chat-content').insertAdjacentHTML('beforeend', template);
-});
+
+
