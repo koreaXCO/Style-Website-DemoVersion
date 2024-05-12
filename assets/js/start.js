de907a42af318b86b61311cacef096d6d8936879
baejiwon <wldnjs7073@naver.com>
2024-05-12 21:32:14 +0900

    카드 3d형식 입체 구현완료

--- assets/js/start.js
+++ assets/js/start.js
@@ -40,3 +40,22 @@ const button = document.querySelector("#goToIndexButton");
 
 // 버튼에 클릭 이벤트 리스너를 추가합니다.
 button.addEventListener("click", goToIndexPage);
+
+//카드 스크립트 
+var container = document.querySelector('.container')
+  var overlay = document.querySelector('.overlay')
+  container.addEventListener('mousemove', function(e){
+    var x = e.offsetX
+    var y = e.offsetY
+    var rotateY = -1/5 * x + 20
+    var rotateX = 4/30 * y - 20
+
+    overlay.style = `background-position : ${x/5 + y/5}%; filter : opacity(${x/200}) brightness(1.2)`
+
+    container.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+  })
+
+  container.addEventListener('mouseout', function(){
+    overlay.style = 'filter : opacity(0)'
+    container.style = 'transform : perspective(350px) rotateY(0deg) rotateX(0deg)'
+  })
